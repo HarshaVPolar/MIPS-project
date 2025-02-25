972c2ba0f26253bce1e77db8999515b86af1bf6c
Vishwa-Pradyumna <vishwapradyumna@gmail.com>
2025-02-25 11:33:19 +0530

    Update MIPS_processor.cpp

--- Assignment 2 Part 1/MIPS_processor.cpp
+++ Assignment 2 Part 1/MIPS_processor.cpp
@@ -1,191 +1,170 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int PC=0;
-char[8][4000] memory='';
-char[32] instr='';
-char[6] opcode='';
-char[5] rs=''; 
-char[5] rt='';
-char[5] rd='';
-char[6] function='';
-char[5] shftamt='';
-char[16] imm='';
-char[26] jmp='';
-char[32][32] regs = {'0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0'}; 
-int rsvalue=0;
-int rtvalue=0;
-int immvalue=0;
+int PC = 0;
+string memory[8][4000];
+string instr;
+string opcode, rs, rt, rd, func, shftamt, imm, jmp, writedata, data;
+string regs[32] = {"0"};
+int rsvalue = 0, rtvalue = 0, immvalue = 0, rdvalue = 0;
+int regdst = 0, branch = 0, memread = 0, memtoreg = 0, memwrite = 0, regwrite = 0, alusrc = 0, zero = 0, alures = 0;
+string aluop = "";
+string aluin = "";
 
-//0 = const value 0, 1 = assembler temporary, 2-3 = procedure return values, 4-7 = procedure arguements, 8-15 = tempvariables, 16-23 = saved variables, 24-25 = temp variables, 26-27 = OS temps, 28 = global pointer, 29 = stack pointer, 30 = frame pointer, 31 = procedure return address 
-
-int regdst=0;
-int branch = 0;
-int memread = 0;
-int memtoreg = 0;
-char[2] aluop = "";
-int memwrite = 0;
-int regwrite = 0;
-int alusrc = 0;
-
-
-
-
-string decimalToTwosComplement(int num, int size) {             //This is necessary to store values in registers or memory 
+string decimalToTwosComplement(int num, int size) {
     if (num >= 0) {
-        string binary = bitset<32>(num).to_string();
-        return binary.substr(32 - size);
+        return bitset<32>(num).to_string().substr(32 - size);
     } else {
-        string binary = bitset<32>(num & ((1 << size) - 1)).to_string();
-        return binary.substr(32 - size);
+        return bitset<32>(num & ((1 << size) - 1)).to_string().substr(32 - size);
     }
 }
 
-int twosComplementToDecimal(const string& binary) {             //This is necessary to do any alu operations
-    int length = binary.length();
+int twosComplementToDecimal(const string& binary) {
     if (binary[0] == '0') {
         return stoi(binary, nullptr, 2);
     }
-    return stoi(binary, nullptr, 2) - (1 << length);
+    return stoi(binary, nullptr, 2) - (1 << binary.length());
 }
 
-void fetch(){
-    cout<<"Fetching instruction"<<endl;
+void execute_jump() {
+    int jumpAddress = (PC & 0xF0000000) | (stoi(jmp, nullptr, 2) << 2);
+    cout << "Jumping to address: " << jumpAddress << endl;
+    PC = jumpAddress;
+}
+
+
+void fetch() {
+    cout << "Fetching instruction" << endl;
     instr = "";
-    for(int i=PC;i<PC+4;i++){
-        instr = instr + memory[i];
+    for (int i = PC; i < PC + 4; i++) {
+        instr += memory[i / 4000][i % 4000];
     }
-    cout << "Instruction :"<<instr<<endl;
-
-    opcode = "";
-    rs = "";
-    rt = "";
-    rd = "";
-    function = "";
-    shftamt = "";
-    imm = "";
-    jmp = "";
+    cout << "Instruction: " << instr << endl;
 }
 
-void decode(){
-    cout<<"Decodeing instructions"<<endl;
-    opcode = instr[0:6];
-    rs = instr[6:11];
-    rt= instr[11:16];
-    rd= instr[16:21];
-    shftamt= instr[21:26];
-    function= instr[26:32];
-    imm= instr[16:32];
-    jmp= instr[6:32];
+void decode() {
+    cout << "Decoding instruction" << endl;
+    opcode = instr.substr(0, 6);
+    rs = instr.substr(6, 5);
+    rt = instr.substr(11, 5);
+    rd = instr.substr(16, 5);
+    shftamt = instr.substr(21, 5);
+    func = instr.substr(26, 6);
+    imm = instr.substr(16, 16);
+    jmp = instr.substr(6, 26);
+
+    rsvalue = twosComplementToDecimal(regs[stoi(rs, nullptr, 2)]);
+    rtvalue = twosComplementToDecimal(regs[stoi(rt, nullptr, 2)]);
+    immvalue = twosComplementToDecimal(imm);
 
     if (opcode == "101011") {
         cout << "STOR:" << endl;
-        rsvalue = twosComplementToDecimal(Reg[stoi(rs, nullptr, 2)]);
-        rtvalue = twosComplementToDecimal(Reg[stoi(rt, nullptr, 2)]);
+        rsvalue = twosComplementToDecimal(regs[stoi(rs, nullptr, 2)]);
+        rtvalue = twosComplementToDecimal(regs[stoi(rt, nullptr, 2)]);
         int temp = twosComplementToDecimal(imm);
-        imm = decimalToTwosComplement(temp, 32);
+        imm = decimalToTwosComplement(temp,32);
         immvalue = twosComplementToDecimal(imm);
-        cout << "Value of RS: " << rsvalue << " Value of RT: " << rtvalue << " Value of Imm: " << immvalue << endl;
-    } 
+        cout << "Value of RS: " << rsvalue << "Value of RT: " << rtvalue << "Value of Imm: " << immvalue << endl;
+    }
     else if (opcode == "100011") {
         cout << "LOAD:" << endl;
-        rsvalue = twosComplementToDecimal(Reg[stoi(rs, nullptr, 2)]);
+        rsvalue = twosComplementToDecimal(regs[stoi(rs, nullptr, 2)]);
         int temp = twosComplementToDecimal(imm);
-        imm = decimalToTwosComplement(temp, 32);
+        imm = decimalToTwosComplement(temp,32);
         immvalue = twosComplementToDecimal(imm);
-        cout << "Value of RS: " << rsvalue << " Value of RT: " << rtvalue << " Value of Imm: " << immvalue << endl;
-    } 
+        cout << "Value of RS: " << rsvalue << "Value of RT: " << rtvalue << "Value of Imm: " << immvalue << endl;
+    }
     else if (opcode == "000000") {
-        if (funct == "100000") {
+        if (func == "100000") {
             cout << "ADD:" << endl;
-            rsvalue = twosComplementToDecimal(Reg[stoi(rs, nullptr, 2)]);
-            rtvalue = twosComplementToDecimal(Reg[stoi(rt, nullptr, 2)]);
-            cout << "Value of RS: " << rsvalue << " Value of RT: " << rtvalue << endl;
-        } 
-        else if (funct == "100010") {
-            cout << "SUB:" << endl;
-            rsvalue = twosComplementToDecimal(Reg[stoi(rs, nullptr, 2)]);
-            rtvalue = twosComplementToDecimal(Reg[stoi(rt, nullptr, 2)]);
-            cout << "Value of RS: " << rsvalue << " Value of RT: " << rtvalue << endl;
+            rsvalue = twosComplementToDecimal(regs[stoi(rs, nullptr, 2)]);
+            rtvalue = twosComplementToDecimal(regs[stoi(rt, nullptr, 2)]);
+            cout << "Value of RS: " << rsvalue << "Value of RT: " << rtvalue << endl;
         }
-    } 
+        else if (func == "100010") {
+            cout << "SUB:" << endl;
+            rsvalue = twosComplementToDecimal(regs[stoi(rs, nullptr, 2)]);
+            rtvalue = twosComplementToDecimal(regs[stoi(rt, nullptr, 2)]);
+            cout << "Value of RS: " << rsvalue << "Value of RT: " << rtvalue << endl;
+        }
+    }
     else if (opcode == "011100") {
         cout << "MUL:" << endl;
-        funct = "000010";
-        rsvalue = twosComplementToDecimal(Reg[stoi(rs, nullptr, 2)]);
-        rtvalue = twosComplementToDecimal(Reg[stoi(rt, nullptr, 2)]);
-        cout << "Value of RS: " << rsvalue << " Value of RT: " << rtvalue << endl;
-    } 
+        func = "000010";
+        rsvalue = twosComplementToDecimal(regs[stoi(rs, nullptr, 2)]);
+        rtvalue = twosComplementToDecimal(regs[stoi(rt, nullptr, 2)]);
+        cout << "Value of RS: " << rsvalue << "Value of RT: " << rtvalue << endl;
+    }
     else if (opcode == "000100") {
         cout << "BEQ:" << endl;
-        rsvalue = twosComplementToDecimal(Reg[stoi(rs, nullptr, 2)]);
-        rtvalue = twosComplementToDecimal(Reg[stoi(rt, nullptr, 2)]);
+        rsvalue = twosComplementToDecimal(regs[stoi(rs, nullptr, 2)]);
+        rtvalue = twosComplementToDecimal(regs[stoi(rt, nullptr, 2)]);
         int temp = twosComplementToDecimal(imm);
-        imm = decimalToTwosComplement(temp, 32);
+        imm = decimalToTwosComplement(temp,32);
         immvalue = twosComplementToDecimal(imm);
-        cout << "Value of RS: " << rsvalue << " Value of RT: " << rtvalue << " Value of Imm: " << immvalue << endl;
-    } 
+        cout << "Value of RS: " << rsvalue << "Value of RT: " << rtvalue << "Value of Imm: " << immvalue << endl;
+    }
     else if (opcode == "000101") {
         cout << "BNE:" << endl;
-        rsvalue = twosComplementToDecimal(Reg[stoi(rs, nullptr, 2)]);
-        rtvalue = twosComplementToDecimal(Reg[stoi(rt, nullptr, 2)]);
+        rsvalue = twosComplementToDecimal(regs[stoi(rs, nullptr, 2)]);
+        rtvalue = twosComplementToDecimal(regs[stoi(rt, nullptr, 2)]);
         int temp = twosComplementToDecimal(imm);
         imm = decimalToTwosComplement(temp, 32);
         immvalue = twosComplementToDecimal(imm);
-        cout << "Value of RS: " << rsvalue << " Value of RT: " << rtvalue << " Value of Imm: " << immvalue << endl;
+        cout << "Value of RS: " << rsvalue << "Value of RT: " << rtvalue << "Value of Imm: " << immvalue << endl;
     } 
     else if (opcode == "000010") {
         cout << "JUMP:" << endl;
-        jmp = "0000" + jmp + "00";
-        immvalue = twosComplementToDecimal(jmp);
+        execute_jump();
+        //jmp = "0000" + jmp + "00";
+        //immvalue = twosComplementToDecimal(jmp);
     }
-        
 }
 
 void ctrl_circuit() {
     if (opcode == "101011") {
-        regdst = 2;
-        branch = 0;
-        memread = 0;
-        memtoreg = 1;
+        regdst = 2; 
+        branch = 0; 
+        memread = 0; 
+        memtoreg = 1; 
         aluop = "00";
-        memwrite = 1;
-        alusrc = 1;
+        memwrite = 1; 
+        alusrc = 1; 
         regwrite = 0;
     } 
     else if (opcode == "100011") {
-        regdst = 0;
-        branch = 0;
-        memread = 1;
-        memtoreg = 1;
+        regdst = 0; 
+        branch = 0; 
+        memread = 1; 
+        memtoreg = 1; 
         aluop = "00";
-        memwrite = 0;
-        alusrc = 1;
+        memwrite = 0; 
+        alusrc = 1; 
         regwrite = 1;
     } 
     else if (opcode == "000000" || opcode == "011100") {
-        regdst = 1;
-        branch = 0;
-        memread = 0;
-        memtoreg = 0;
+        regdst = 1; 
+        branch = 0; 
+        memread = 0; 
+        memtoreg = 0; 
         aluop = "10";
-        memwrite = 0;
-        alusrc = 0;
+        memwrite = 0; 
+        alusrc = 0; 
         regwrite = 1;
     } 
     else if (opcode == "000100") {
-        regdst = 2;
-        branch = 1;
-        memread = 0;
-        memtoreg = 1;
+        regdst = 2; 
+        branch = 1; 
+        memread = 0; 
+        memtoreg = 1; 
         aluop = "01";
-        memwrite = 0;
-        alusrc = 0;
+        memwrite = 0; 
+        alusrc = 0; 
         regwrite = 0;
-        if (rsval == rtval) {
+        if ((rsvalue == rtvalue)) {
             zero = 1;
         }
-    } 
+    }
     else if (opcode == "000101") {
         regdst = 2;
         branch = 1;
@@ -195,30 +174,108 @@ void ctrl_circuit() {
         memwrite = 0;
         alusrc = 0;
         regwrite = 0;
-        if (rsval != rtval) {
+        if (rsvalue != rtvalue) {
             zero = 1;
         }
     } 
     else if (opcode == "000010") {
-        regdst = 2;
-        branch = 1;
-        memread = 0;
-        memtoreg = 1;
+        regdst = 2; 
+        branch = 1; 
+        memread = 0; 
+        memtoreg = 1; 
         aluop = "00";
-        memwrite = 0;
-        alusrc = 1;
+        memwrite = 0; 
+        alusrc = 1; 
         regwrite = 0;
         cout << string(75, '-') << endl;
         return;
     }
 
     if (regdst == 1) {
-        rdval = stoi(rd, nullptr, 2);
-    } 
-    else if (regdst == 0) {
-        rdval = stoi(rt, nullptr, 2);
+        rdvalue = stoi(rd, nullptr, 2);
     }
-
+    else if (regdst == 0) {
+        rdvalue = stoi(rt, nullptr, 2);
+    }
 }
 
+void alu_ctrl() {
+    if (aluop == "00") {
+        aluin = "010";
+    }
+    else if (aluop == "10" && func == "100000") {
+        aluin = "010";
+    }
+    else if (aluop == "10" && func == "100010") {
+        aluin = "011";
+    }
+    else if (aluop == "10" && func == "000010") {
+        aluin = "111";
+    }
+    else if (aluop == "01") {
+        aluin = "100";
+    }
+}
+
+void alu() {
+    cout << "ALU ---->" << endl;
+    if (aluin == "010") {
+        if (alusrc == 1) {
+            alures = rsvalue + immvalue;
+            cout << "After Add: " << alures << endl;
+        }
+        else if (alusrc == 0) {
+            alures = rsvalue + rtvalue;
+            cout << "After Add: " << alures << endl;
+        }
+    }
+    else if (aluin == "011") {
+        if (alusrc == 0) {
+            alures = rsvalue - rtvalue;
+            cout << "After Sub: " << alures << endl;
+        }
+    }
+    else if (aluin == "111") {
+        if (alusrc == 0) {
+            alures = rsvalue*rtvalue;
+            cout << "After Mul: " << alures << endl;
+        }
+    }
+    else if (aluin == "100") {
+        immvalue = immvalue*4;
+        cout << "Immediate: " << immvalue << endl;
+    }
+}
+
+/* void memory_access() {
+    if (memwrite == 0 && memread == 1) {
+        cout << "Memory Acess ----->" << endl;
+        string var;
+        int i=0;
+        while(i<4) {
+            var += M[alures+i];
+        }
+
+    }
+} */
+
+void writeback() {
+    if (regwrite == 1) {
+        cout << "Writeback ----->" << endl;
+        cout << "rd: " << rdvalue << endl;
+        cout << "MemtoReg: " << memtoreg << endl;
+        if (memtoreg == 1) {
+            cout << "Data: " << data << endl;
+            writedata = data;
+            regs[rdvalue] = writedata;
+            cout << "After writeback, value at destination: " << regs[rdvalue] << endl;
+        }
+        else {
+            cout << "Write data: " << decimalToTwosComplement(alures,32) << endl;
+            writedata = alures;
+            regs[rdvalue] = decimalToTwosComplement(writedata,32);
+            cout << "After writeback, value at destination: " << regs[rdvalue] << endl;
+        }
+    }
+}
 
